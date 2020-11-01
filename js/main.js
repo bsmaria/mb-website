37bea1e91fb3685660b215c872a2c6731566136e
bsmaria <bmaria.contact@gmail.com>
2020-11-01 18:22:34 -0300

    1 de noviembre

--- js/main.js
+++ js/main.js
@@ -1,12 +1,23 @@
 $( document ).ready(function() {
-  console.log( "ready!" );
+
+  document.getElementById("scroll-secuence-inside").style.height = "0%";
+
   window.onscroll = function() {showProgress()};
-   function showProgress() {
+
+      function showProgress() {
       var scrollCalculate = document.body.scrollTop || document.documentElement.scrollTop;
       var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
       var scrolled = (scrollCalculate / height) * 100;
       document.getElementById("scroll-secuence-inside").style.height = scrolled + "%";
+
    }
+   
+$('.j-galery').slick({
+   dots: true,
+   infinite: true,
+   speed: 300,
+   slidesToShow: 1
+});
 
 });
 
