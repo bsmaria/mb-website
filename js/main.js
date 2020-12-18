4e4d9d912e11a6d586f0e6207a71f619e05dcd99
bsmaria <bmaria.contact@gmail.com>
2020-12-18 13:06:21 -0300

    last commit

--- js/main.js
+++ js/main.js
@@ -2,22 +2,19 @@ $( document ).ready(function() {
 
   document.getElementById("scroll-secuence-inside").style.height = "0%";
 
-  window.onscroll = function() {showProgress()};
+  window.onscroll = function() { showProgress() };
 
       function showProgress() {
-      var scrollCalculate = document.body.scrollTop || document.documentElement.scrollTop;
-      var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-      var scrolled = (scrollCalculate / height) * 100;
+      let scrollCalculate = document.body.scrollTop || document.documentElement.scrollTop;
+      let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+      let scrolled = (scrollCalculate / height) * 100;
       document.getElementById("scroll-secuence-inside").style.height = scrolled + "%";
+
 
    }
    
-$('.j-galery').slick({
-   dots: true,
-   infinite: true,
-   speed: 300,
-   slidesToShow: 1
-});
+
+
 
 });
 
