515421be357f3a35622b7c69f320c39700408ce1
bsmaria <bmaria.contact@gmail.com>
2021-03-23 16:04:40 -0300

    commit

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
-$( document ).ready(function() {
+$(document).ready(function () {
 
-  document.getElementById("scroll-secuence-inside").style.height = "0%";
+   document.getElementById("scroll-secuence-inside").style.height = "0%";
 
-  window.onscroll = function() { showProgress() };
+   window.onscroll = function () {
+      showProgress()
+   };
 
-      function showProgress() {
+   function showProgress() {
       let scrollCalculate = document.body.scrollTop || document.documentElement.scrollTop;
       let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
       let scrolled = (scrollCalculate / height) * 100;
@@ -12,9 +14,8 @@ $( document ).ready(function() {
 
 
    }
-   
+
 
 
 
 });
-
