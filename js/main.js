c292fbb39cc416443526ee80b20724b603d7f55d
bsmaria <bmaria.contact@gmail.com>
2021-03-24 17:59:33 -0300

    base styling

--- js/main.js
+++ js/main.js
@@ -1,20 +1,5 @@
 $(document).ready(function () {
 
-   document.getElementById("scroll-secuence-inside").style.height = "0%";
-
-   window.onscroll = function () {
-      showProgress()
-   };
-
-   function showProgress() {
-      let scrollCalculate = document.body.scrollTop || document.documentElement.scrollTop;
-      let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-      let scrolled = (scrollCalculate / height) * 100;
-      document.getElementById("scroll-secuence-inside").style.height = scrolled + "%";
-
-
-   }
-
 
 
 
